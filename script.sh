d3b5fa29da99b4021e72fd4f2ed8190bf12cce3b
salman.saeed <salman.saeed@northbaysolutions.net>
2019-10-28 03:34:19 +0500

    latest code

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!bin/bash
- cd /usr/share/nginx/html/nodejs
+ cd /nodejs-app
  curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
  yum -y install nodejs > /dev/null 2> /dev/null < /dev/null &
  npm start > /dev/null 2> /dev/null < /dev/null &
